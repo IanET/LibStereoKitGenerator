e06fd35bf3088da9df2ea2d9ba3dc93e59f7bfd6
4thjuly <4thjuly@users.noreply.github.com>
2021-12-05 18:51:13 -1000

    ...

--- LibStereoKit.jl
+++ LibStereoKit.jl
@@ -8,6 +8,11 @@ using CEnum
 
 StereoKitC = joinpath(@__DIR__, "StereoKitC.dll")
 
+function __init__()
+    # TODO - make a proper 'artifact'
+    chmod(StereoKitC, filemode(StereoKitC) | 0o755) # dll needs to executable
+end
+
 
 const uint64_t = Culonglong
 
--- prologue.jl
+++ prologue.jl
@@ -3,3 +3,8 @@
 #
 
 StereoKitC = joinpath(@__DIR__, "StereoKitC.dll")
+
+function __init__()
+    # TODO - make a proper 'artifact'
+    chmod(StereoKitC, filemode(StereoKitC) | 0o755) # dll needs to executable
+end
